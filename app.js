12112714d192d31931b3e40d0d011f813b5dc352
musetea <dev0019@naver.com>
2020-07-30 02:11:07 +0900

    반응형 헤더 타이틀 작업

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const path  = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get('/', (req, res) => { res.sendFile('./public/index.html') }); 
 
 app.listen(4001, () => { console.log('Response Web Server Port 4001 is Running...')})
